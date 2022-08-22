3cbd2c1bb4dd9ab89d6649f59258dc3367fbd965
PlaceblockHD <felix.weglehner@gmx.de>
2022-08-23 01:00:38 +0200

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.24")
     testCompileOnly("org.projectlombok:lombok:1.18.24")
     compileOnly("io.schark:ScharkDesign:1.1.1")
-    compileOnly("net.kyori:adventure-platform-bungeecord:4.1.1")
+    compileOnly("net.kyori:adventure-platform-bungeecord:4.1.2")
     testCompileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
     testCompileOnly("io.github.waterfallmc:waterfall-api:1.18-R0.1-SNAPSHOT")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
