e02aade5ac45a241cb824c04b8029f49c675bc0a
PlaceblockHD <felix.weglehner@gmx.de>
2022-08-23 02:35:05 +0200

    Fixed Bugs

--- src/main/java/de/placeblock/commandapi/bridge/paper/AbstractPaperCommandBridge.java
+++ src/main/java/de/placeblock/commandapi/bridge/paper/AbstractPaperCommandBridge.java
@@ -16,7 +16,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 @SuppressWarnings("unused")
-public abstract class AbstractPaperCommandBridge<PL extends JavaPlugin, P> extends Command implements CommandBridge<Player, P, CommandSender, PaperCommandSource<P>>, CommandExecutor, TabCompleter {
+public abstract class AbstractPaperCommandBridge<PL extends JavaPlugin, P> extends Command implements CommandBridge<Player, P, CommandSender, PaperCommandSource<P>> {
     @Getter
     private final CommandAPICommand<PaperCommandSource<P>> commandAPICommand;
 
@@ -55,21 +55,22 @@ public abstract class AbstractPaperCommandBridge<PL extends JavaPlugin, P> exten
         plugin.getServer().getCommandMap().register(label, "commandapi", this);
     }
 
+
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+    public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
         P lobbyPlayer = null;
         if (sender instanceof Player player) {
             lobbyPlayer = this.getCustomPlayer(player);
         }
         PaperCommandSource<P> source = new PaperCommandSource<>(lobbyPlayer, sender);
-        ParseResults<PaperCommandSource<P>> parseResults = this.commandAPICommand.parse(source, label + " " + String.join(" ", args));
+        ParseResults<PaperCommandSource<P>> parseResults = this.commandAPICommand.parse(source, commandLabel + " " + String.join(" ", args));
         this.commandAPICommand.execute(parseResults);
         return true;
     }
 
     @Override
-    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        String buffer = label + " " + String.join(" ", args);
+    public @NotNull List<String> tabComplete(@NotNull CommandSender sender, @NotNull String alias, @NotNull String[] args) {
+        String buffer = alias + " " + String.join(" ", args);
         P customPlayer = null;
         if (sender instanceof Player player) {
             customPlayer = this.getCustomPlayer(player);
@@ -77,4 +78,5 @@ public abstract class AbstractPaperCommandBridge<PL extends JavaPlugin, P> exten
         ParseResults<PaperCommandSource<P>> parseResults = this.commandAPICommand.parse(new PaperCommandSource<>(customPlayer, sender), buffer);
         return this.commandAPICommand.getSuggestions(parseResults);
     }
+
 }
